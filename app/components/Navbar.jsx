bd9ad1d4b5d211704f1cfbe6a7b67fbc7661d189
thegurlalsingh <g0390076@gmail.com>
2025-07-08 17:10:54 +0530

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { PiReadCvLogoBold } from "react-icons/pi";
 import Link from 'next/link'
 
 const Navbar = () => {
@@ -12,6 +13,7 @@ const Navbar = () => {
             <h1 className='text-3xl font-extrabold'>GS</h1>
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <Link href={"https://drive.google.com/file/d/1yBjlJVijvvNy5mdz1oTFkUtPCXCQ6PM1/view?usp=drive_link"}><PiReadCvLogoBold /></Link>
         <Link href={"https://www.linkedin.com/in/gurlal-singh-6933702a4/"}><FaLinkedin /></Link>
         <Link href={"https://github.com/thegurlalsingh"}><FaGithub /></Link>
         <Link href={"https://www.instagram.com/thegurlalsinghh/"}><FaInstagram /></Link>
